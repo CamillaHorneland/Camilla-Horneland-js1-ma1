45fddf69d2cad9b68802e3a646e6e6268d859e07
Camilla <104870685+CamillaHorneland@users.noreply.github.com>
2022-10-20 01:20:01 +0200

    1-3

---
+++ js/script.js
@@ -0,0 +1,54 @@
+const cats = [
+    {
+        name: "Blob",
+        age: 10
+    },
+    {
+        name: "Harold",
+    },
+    {
+        name: "Blurt",
+        age: 21
+    }
+];
+
+//Question 1.
+
+const cat = {
+    complain: function() {
+        console.log("Meow!");
+    }
+};
+cat.complain();
+
+
+
+//Question 2.
+
+const heading = document.querySelector("h3");
+
+heading.innerHTML = "Update heading";
+
+
+//Question 3.
+
+heading.style.fontSize = "2em";
+
+//Question 4.
+
+
+
+//Question 5.
+
+
+
+//Question 6.
+
+
+
+//Question 7.
+
+
+
+
+//Question 8.
